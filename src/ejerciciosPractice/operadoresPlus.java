0f9ffda953b6bf6f1d0409b1c87126bc23c777fa
AlvaroJesusC <cmamanialvarojesus@gmail>
2025-07-29 22:37:24 -0500

    update

--- src/ejerciciosPractice/operadoresPlus.java
+++ src/ejerciciosPractice/operadoresPlus.java
@@ -21,7 +21,28 @@ public class operadoresPlus {
         numero %= 5;
         System.out.println(numero);
 
+        System.out.println("----------------------");
+        //OPERADOR DE INCREMENTO Y DECREMENTO
+        int x = 5;
 
+
+        x++; //autmenta 1
+
+        System.out.println(x);
+
+        int b = 10; //diminuye 1
+
+        b--;
+        System.out.println(b);
+
+        int a = 20;
+        int y;
+
+        //y = --a; // si se poner el prefijo primero osea "--" entonces diminuira 1 no es lo mismo que a++
+        //System.out.println(y);
+
+        y = ++a;
+        System.out.println(y);
 
     }
 }
